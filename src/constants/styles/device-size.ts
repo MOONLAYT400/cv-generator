7b977c6d718a58929e609ffe46cd984b7e5d6cde
Anton <am@metaplan.dev>
2024-10-07 23:18:39 +0300

    Styles constants

---
+++ src/constants/styles/device-size.ts
@@ -0,0 +1,10 @@
+export enum DEVICE_SIZE {
+  ULTRA_HD = "max-width: 3840px",
+  QUAD_HD = "max-width: 2560px",
+  FULL_HD = "max-width: 1920px",
+  DESKTOP_M = "max-width: 1450px",
+  DESKTOP = "max-width: 1280px",
+  LAPTOP = "max-width: 1042px",
+  TABLET = "max-width: 840px",
+  MOBILE = "max-width: 430px"
+}
