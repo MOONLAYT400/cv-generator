1a251f54dc7704257186ce71a9cb699ff7d35732
Eugena <eugena.h12345@gmail.com>
2024-12-12 23:50:07 +0300

    add Paragraphs and breaks, add education into docx

--- src/hooks/useCVGenerator.ts
+++ src/hooks/useCVGenerator.ts
@@ -1,10 +1,10 @@
-import { Document, HeadingLevel, Packer, Paragraph, Table, TableRow, TableCell, WidthType, BorderStyle } from "docx"
+import { Document, HeadingLevel, Packer, Paragraph, Table, TableRow, TableCell, WidthType, BorderStyle, TextRun } from "docx"
 import { saveAs } from "file-saver"
 
 import { ICVParams } from "@/types/cv-data"
 
 export const useCVGenerator = (params: ICVParams): (() => void) => {
-  const { fullName, technologies, experience } = params;
+  const { fullName, technologies, experience, education } = params;
 
   const technologiesString = technologies.map((item) => {
     return ` ${item.name.toString()}`;
@@ -19,9 +19,27 @@ export const useCVGenerator = (params: ICVParams): (() => void) => {
   // projects: Array<IProjectItem>
 
   // TODO implement file structure
-  const some = experience.map((itemExperiense) => {
-    return `${itemExperiense.company} даты: ${itemExperiense.startDate}-${itemExperiense.endDate}\n`
-  })
+  const experienceDescription = experience.map((itemExperiense) => {
+    return new Paragraph({
+      children: [new TextRun({ text: `${itemExperiense.company}`, break: 0 }),
+      new TextRun({ text: `${itemExperiense.role}`, break: 1 }),
+      new TextRun({ text: `даты: ${itemExperiense.startDate}-${itemExperiense.endDate}`, break: 1 }),
+      new TextRun({ text: "Должностные обязанности:", break: 1 }),
+      new TextRun({ text: `${itemExperiense.duties.map((duty) => duty.text)}`, break: 1 })
+      ],
+    })
+  });
+
+  const educationDescription = education.map((education) => {
+    return new Paragraph({
+      children: [new TextRun({ text: `${education.university}`, break: 0 }),
+      new TextRun({ text: `${education.field}`, break: 1 }),
+      new TextRun({ text: `даты: ${education.startDate || ''}-${education.endDate || ''}`, break: 1 }),
+      ],
+    })
+  });
+
+
   const doc = new Document({
     sections: [
       {
@@ -63,11 +81,11 @@ export const useCVGenerator = (params: ICVParams): (() => void) => {
                     },
                   }),
                   new TableCell({
-                    children: [new Paragraph("Опыт работы"),
-                    new Paragraph({
-                      text: some.toString(),
-                    }),
-                    new Paragraph("Образование"),
+                    children: [
+                      new Paragraph({text: "Опыт работы"}),
+                      ...experienceDescription,// заменить копирование на что-то более элегантное, на объединение массивов?
+                      new Paragraph("Образование"),
+                      ...educationDescription,
                     ],
                     width: {
                       size: 6000,
