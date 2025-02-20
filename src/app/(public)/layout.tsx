c95778687c3add3344555f653fcbf79499a6cde7
Eugena <eugena.h12345@gmail.com>
2025-02-20 21:24:36 +0300

    add ThemeProvider

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,20 +1,42 @@
 "use client"
+import { useState, useEffect } from "react";
+import { ThemeProvider } from "styled-components"
+
+import { Themes } from "../../components/ui/theme-section/index"
+import GlobalStyle from '../../styles/global-styled'
+import {themes, defaultTheme, Theme} from '../../styles/themes.styled'
+
+import { ChildrenWrapper, Wrapper } from "./layout.styled"
 
 import { NavWrapper } from "@/components/features/nav-wrapper"
 import { Header } from "@/components/ui/header"
-
-import { ChildrenWrapper, Wrapper } from "./layout.styled"
 
 export default function PublicLayout({
   children
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const [selectedTheme, setSelectedTheme] = useState<Theme>(themes[defaultTheme]);
+  const handleThemeChange = (theme: Theme):void => {
+    setSelectedTheme(theme);
+    localStorage.setItem("current-theme", JSON.stringify(theme));
+  };
+  useEffect(() => {
+    const currentTheme = JSON.parse(localStorage.getItem("current-theme") || '');
+    if (currentTheme) {
+      setSelectedTheme(currentTheme);
+    }
+  }, []);
+
   return (
     <Wrapper>
       <NavWrapper>
-        <Header />
-        <ChildrenWrapper>{children}</ChildrenWrapper>
+        <ThemeProvider theme={selectedTheme}>
+          <GlobalStyle />
+          <Header/>
+          <Themes handleThemeChange={handleThemeChange}></Themes>
+          <ChildrenWrapper>{children}</ChildrenWrapper>
+        </ThemeProvider>
       </NavWrapper>
     </Wrapper>
   )
